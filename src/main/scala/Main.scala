e1760fccbde49ca0bf0cb01b2b804b4cc7851355
Arseny <st.arseny@yandex.ru>
2020-03-18 21:19:11 +0300

    first version of Animal

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+import mywork.Animal
+
+object Main extends App {
+
+  val a = Animal.cat
+  println(a.eats("meat"))
+
+}
--- src/main/scala/mywork/Animal.scala
+++ src/main/scala/mywork/Animal.scala
@@ -1,11 +1,37 @@
 package mywork
 
-class Animal {
+/**
+ * A simple animal entity
+ * @param aName animal name
+ * @param aSpecies animal species
+ * @param aFood what animal eats
+ */
+class Animal(aName: String, aSpecies: String, aFood: String) {
 
-  var name: String = ""
+  private val name: String = aName
 
-  var species: String = ""
+  private val species: String = aSpecies
 
-  var food: String = ""
+  private val food: String = aFood
+
+  /**
+   * Returns if animal eats certain food
+   * @param food food that animal may or may not eat
+   * @return true if animal eats the food, false otherwise
+   */
+  def eats(food: String): Boolean =
+    ((name equals Animal.cat.name) && (food equals Animal.cat.food)) ||
+      ((name equals Animal.parrot.name) && (food equals Animal.parrot.food)) ||
+      ((name equals Animal.goldfish.name) && (food equals Animal.goldfish.food))
+
+}
+
+object Animal {
+
+  val cat = new Animal("cat", "mammal", "meat")
+
+  val parrot = new Animal("parrot", "bird", "vegetables")
+
+  val goldfish = new Animal("fish", "fish", "plants")
 
 }
